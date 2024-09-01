5f3ae3260bb8cc5360ae3657e2158dce71b3b818
Sanketlohana <124903469+Sanketlohana@users.noreply.github.com>
2024-09-01 12:13:27 +0530

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+st.markdown("<h1 style='text-align: center;'>Dragon Warrior Thief Game</h1>", unsafe_allow_html=True)
+
+# Add some space after the title
+st.text("")
+st.text("")
+
+# Create three columns with custom widths for spacing
+col1, col2, col3 = st.columns([2, 2 ,2])
+
+# Display an image in each column
+with col1:
+    st.image("image1.png", use_column_width=True)
+
+with col2:
+    st.image("image2.png", use_column_width=True)
+    
+with col3:
+    st.image("image3.png", use_column_width=True)
