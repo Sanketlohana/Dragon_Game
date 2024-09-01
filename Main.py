4ea0298b63d350a6052f4a9bc320e16aef6c2263
Sanketlohana <124903469+Sanketlohana@users.noreply.github.com>
2024-09-01 11:29:36 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-files = ["Home" if file == "Main.py"]
+files = ["Home" if file == "main.py" else file for file in files]
 
 st.markdown("<h1 style='text-align: center;'>Dragon Warrior Thief Game</h1>", unsafe_allow_html=True)
 
