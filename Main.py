a4a43cf554665fc94fa0f204368dce5aa445a0c2
Sanketlohana <124903469+Sanketlohana@users.noreply.github.com>
2024-09-01 11:45:28 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.sidebarheader("Home")
+st.sidebar.header("Home")
 
 
 st.markdown("<h1 style='text-align: center;'>Dragon Warrior Thief Game</h1>", unsafe_allow_html=True)
